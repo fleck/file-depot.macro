319a83e865450f1555ee273c8d37ebf6a7c03bbc
Jonathan <jonfleck88@gmail.com>
2021-06-08 22:25:21 -0400

    update test

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -9,9 +9,9 @@ pluginTester({
   tests: {
     "transform function call to string": {
       code: `
-        import {createVariation} from '../../file-depot.macro';
+        import {variation} from '../../file-depot.macro';
 
-        createVariation({some: "transform"})
+        variation({some: "transform"})
       `,
       output: '"e4e7d76bd2b16c69a7adbed0667a8b0a"',
       setup: () => {
