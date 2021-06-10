0e5aa4d849133ed835146a1f5078d17cb042fd34
Jonathan <jonfleck88@gmail.com>
2021-06-09 23:14:00 -0400

    add test for types

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,6 +2,14 @@ import pluginTester from "babel-plugin-tester"
 import plugin from "babel-plugin-macros"
 import fs from "fs-extra"
 import { transformsFilePath } from "../src"
+import { variant } from "../src/"
+
+test.skip("types", () => {
+  // @ts-expect-error
+  variant("invalidTransform")
+
+  variant("avif", { rotate: 90 }, { resize: { width: 200 } })
+})
 
 pluginTester({
   plugin,
