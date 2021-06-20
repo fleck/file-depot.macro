fe7ebaff02068cb6032b7389bcfafd7544fdbb3c
Jonathan <jonfleck88@gmail.com>
2021-06-19 21:42:49 -0400

    add tests for types

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,10 +1,10 @@
 import pluginTester from "babel-plugin-tester"
-import plugin, { MacroError } from "babel-plugin-macros"
+import plugin from "babel-plugin-macros"
 import fs from "fs-extra"
-import { transformsFilePath } from "../src"
-import { variant } from "../src/"
+import { transformsFilePath, variant, Transform } from "../src"
+import sharp from "sharp"
 
-test.skip("types", () => {
+test.skip("variant types", () => {
   variant("rotate")
 
   // @ts-expect-error
@@ -14,6 +14,37 @@ test.skip("types", () => {
 
   // positional args
   variant("avif", { threshold: [90, { grayscale: true }] })
+
+  variant("flatten", { jpeg: { optimiseScans: true }, threshold: 90 })
+
+  // @ts-expect-error don't allow undefined to be passed
+  variant({ avif: undefined })
+})
+
+test.skip("other types", () => {
+  const v = {}
+
+  const variations = (v as unknown) as { [key: string]: Transform[] }
+
+  const transforms = variations["fakeVariationKey"]
+
+  if (!transforms) return
+
+  let s = sharp()
+
+  transforms.forEach(transform => {
+    if (typeof transform === "string") {
+      transform
+      s = s[transform]()
+    } else {
+      Object.entries(transform).forEach(([method, args]) => {
+        // @ts-expect-error not sure how to properly type this 🤷‍♂️
+        s = s[method](args)
+      })
+    }
+  })
+
+  return s
 })
 
 pluginTester({
@@ -27,17 +58,6 @@ pluginTester({
         variant({some: "transform"})
       `,
       output: '"b763fa71f19b62fd4dde15ee14dfb9f3"',
-      setup: () => {
-        fs.removeSync(transformsFilePath)
-      }
-    },
-    "should give error if undefined": {
-      code: `
-        import {variant} from '../../file-depot.macro';
-
-        variant({avif: undefined})
-      `,
-      error: (MacroError as unknown) as Error,
       setup: () => {
         fs.removeSync(transformsFilePath)
       }
